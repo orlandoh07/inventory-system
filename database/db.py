20955146c22ade2c6f4f487e61636a618e5f1608
Orlando Herrera <orlandoherrera2191@gmail.com>
2026-05-20 21:22:55 -0400

    database design

--- database/db.py
+++ database/db.py
@@ -1,1 +1,37 @@
+import sqlite3
 
+def get_connection():
+    connection = sqlite3.connect("database/inventory.db")
+    return connection
+
+
+
+def create_tables():
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS categories (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS products (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            price REAL,
+            quantity INT,
+            category_id INT
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT,
+            password TEXT
+        )
+    """)
+    connection.commit()
+    connection.close()
